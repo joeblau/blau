73f0cda28b91225a8010524c20184ff8e14543bd
Joe Blau <im@joeblau.com>
2025-06-05 07:57:47 -0700

    feat: update portfolio and login form

--- apps/www/src/app/(auth)/login/page.tsx
+++ apps/www/src/app/(auth)/login/page.tsx
@@ -1,8 +1,22 @@
 import { LoginForm } from "@/components/blau/login-form";
+import { ModeToggle } from "@/components/blau-ui/mode-toggle";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { HomeIcon } from "lucide-react";
 
-export default function Home() {
+export default function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="absolute left-6 top-6 md:left-10 md:top-10">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/">
+            <HomeIcon className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="absolute right-6 top-6 md:right-10 md:top-10">
+        <ModeToggle />
+      </div>
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Web3Provider } from "@/components/provider/web3-provider";
-
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Web3Provider>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </Web3Provider>
       </body>
     </html>
